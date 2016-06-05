ea153f1cf2651066b3cff045f35e01df45bc9856
Manabu Tokunaga <manabubeach@gmail.com>
2016-06-04 21:09:58 -0700

    Device  List with Pull Down, plus a (+) button

--- client/app/ctrls/main.js
+++ client/app/ctrls/main.js
@@ -7,15 +7,14 @@ app.controller = function() {
     var ctrl = this;
 	ctrl.list = app.storage.get();
     ctrl.listCount = m.prop(0);
-    ctrl.devNames = deviceNames.map(function(item, index) { return new app.DevName(item, index)});
-    console.log(ctrl.devNames[0].name());
+    ctrl.approvedDevices = approvedDevices;
      
 	// Update with props
 	ctrl.list = ctrl.list.map(function (item) {
 		return new app.Device(item);
 	});
     
-    ctrl.selectedDev = m.prop(ctrl.devNames[0]);
+    ctrl.selectedDev = m.prop(0);
     
     ctrl.addNew = function addNew () {
         var that = this;
@@ -37,13 +36,13 @@ var Select2 = {
         var selectedId = attrs.value;
         //Create a Select2 progrssively enhanced SELECT element
         return m("select", {config: Select2.config(attrs)}, [
-            attrs.data.map(function(item) {
-                var args = {value: item.id()};
+            attrs.data.map(function(item, index) {
+                var args = {value: index};
                 //    Set selected option
                 if(item.id == selectedId) {
                     args.selected = "selected";
                 }
-                return m("option", args, item.name());
+                return m("option", args, item);
             })
         ]);
     },
@@ -66,9 +65,9 @@ var Select2 = {
                             var id = el.select2("val");
                             m.startComputation();
                             //Set the value to the selected option
-                            ctrl.data.map(function(d){
-                                if(d.id() == id) {
-                                    ctrl.value(d);
+                            ctrl.data.map(function(d, index){
+                                if(index == id) {
+                                    ctrl.value(id);
                                 }
                             });
 
@@ -78,7 +77,7 @@ var Select2 = {
                             m.endComputation();
                         });
                 }
-                el.val(ctrl.value).trigger("change");
+                el.val(ctrl.value()).trigger("change");
             } else {
                 console.warn('ERROR: You need jquery and Select2 in the page');    
             }
--- client/app/views/device-view.js
+++ client/app/views/device-view.js
@@ -1,33 +1,37 @@
 'use strict';
 var app = app || {};
 
-app.view = function(ctrl) {
+app.view = function (ctrl) {
     return m("header#header", [
-        m("h1", "Devices"),
-        m("br"),
-        m("p", "Total devices: ", ctrl.listCount())
+        m("h1", "Devices")
     ],
-    m("button#add", {onclick: ctrl.addNew.bind(ctrl)}, "Add"),
-    m.component(Select2, {
-        data: ctrl.devNames, value: ctrl.selectedDev, onchange: function() {}
-    }),
-    m("p", " "),
-    m("table", {class: "table table-striped"},  [
-        m("thead", {class: "thead-inverse"}, [
-        m("tr", [
-            m("th", "Index"),
-            m("th", "Device"),
-            m("th", "Legion"),
-            m("th", "Diameter"),
-            m("th", "Length")
-        ])]),
-        m("tbody", [
-            m("tr", [
-            m("td", "1"),
-            m("td", "Stent")
-            ])
+        m("p", " "),
+        m("table", { class: "table table-striped" }, [
+            m("thead", { class: "thead-inverse" }, [
+                m("tr", [
+                    m("th", "#"),
+                    m("th", "Device"),
+                    m("th", "Legions"),
+                    m("th", "Diameter"),
+                    m("th", "Length")
+                ])]),
+            m("tbody", [
+                ctrl.list.map(function (item) {
+                    return m("tr", [
+                        m("td", [m("button", "x " + item.id)]),
+                        m("td", [
+                            m.component(Select2, {
+                                data: ctrl.approvedDevices, value: ctrl.selectedDev, onchange: function () { }
+                            })
+                        ]),
+                        m("td", "L"),
+                        m("td", "1.0"),
+                        m("td", "1.5")
+                    ])
+                })
+            ]),
+            m("button#add", { onclick: ctrl.addNew.bind(ctrl) }, "+"),
+            m("p", "Total devices: ", ctrl.listCount())
         ])
-       
-    ])
     ); // top
 }
