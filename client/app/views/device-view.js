35ae528c5318262bd6e2084c3dd62473c188669a
Manabu Tokunaga <manabubeach@gmail.com>
2016-06-04 19:13:30 -0700

    Added Bootstrap full dist as bower components

--- client/app/views/device-view.js
+++ client/app/views/device-view.js
@@ -12,19 +12,22 @@ app.view = function(ctrl) {
         data: ctrl.devNames, value: ctrl.selectedDev, onchange: function() {}
     }),
     m("p", " "),
-    m("table", {class: "table"},  [
-        m("thead", [
+    m("table", {class: "table table-striped"},  [
+        m("thead", {class: "thead-inverse"}, [
         m("tr", [
-            m("td", "Index"),
-            m("td", "Device"),
-            m("td", "Legion"),
-            m("td", "Diameter"),
-            m("td", "Length")
+            m("th", "Index"),
+            m("th", "Device"),
+            m("th", "Legion"),
+            m("th", "Diameter"),
+            m("th", "Length")
         ])]),
-       m("tr", [
+        m("tbody", [
+            m("tr", [
             m("td", "1"),
             m("td", "Stent")
-        ]),  
+            ])
+        ])
+       
     ])
     ); // top
 }
